8c2fcf874511b6c49b6fac6386ec0810ef3abe41
Ezequiel <eze.arroyo00@outlook.com>
2024-04-18 02:43:29 -0300

    rama-semiFinal

---
+++ JS/style.js
@@ -0,0 +1,7 @@
+/*const hoverUno = document.querySelector("#hoverUno");
+
+const hoverDos = document.querySelector("#hoverDos");
+
+hoverUno.addEventListener("mouseenter",()=> {
+        hoverUno.style.backgroundImage = "url(/Archivos/imagenes-card-final/imagenes-hover/imagen-1.png)";
+})*/
