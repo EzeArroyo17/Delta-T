f88c1191a19589da00efa9de747ef5e0613992b8
Ezequiel <eze.arroyo00@outlook.com>
2024-04-20 18:30:22 -0300

    ramaOk

--- JS/style.js
+++ JS/style.js
@@ -1,7 +1,24 @@
-/*const hoverUno = document.querySelector("#hoverUno");
-
+const hoverUno = document.querySelector("#hoverUno");
+const hover = document.querySelector(".hover")
 const hoverDos = document.querySelector("#hoverDos");
 
-hoverUno.addEventListener("mouseenter",()=> {
+const verMas = document.querySelector("#verMas");
+/*
+function agregarBoton (){
+        verMas.innerHTML=`<input type="button" value="Ver Mas"></input>`
+        const claseAAgregar = 'inputClase';
+        verMas.classList.add(claseAAgregar);   
+}
+agregarBoton(verMas)
+const inputClase = document.querySelector(".inputClase")
+
+inputClase.addEventListener("click",()=>{
         hoverUno.style.backgroundImage = "url(/Archivos/imagenes-card-final/imagenes-hover/imagen-1.png)";
+        hoverUno.style.backgroundRepeat= "no-Repeat" 
+        hoverUno.style.backgroundPosition= "center" ;
+        hoverUno.style.backgroundSize="auto" ;
+        hoverUno.style.opacity= "1" ;
+        hoverUno.style.cursor= "pointer" ;
+        hover.style.opacity= "1" 
+        hover.style.transition="transform 0.3s, opacity 0.3s" 
 })*/
