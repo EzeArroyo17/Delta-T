ec6779564c422e9ffce4db33feed2def07952852
Ezequiel <eze.arroyo00@outlook.com>
2024-04-26 19:12:56 -0300

    ante ultimo commit

--- JS/style.js
+++ JS/style.js
@@ -1,24 +1,13 @@
-const hoverUno = document.querySelector("#hoverUno");
-const hover = document.querySelector(".hover")
-const hoverDos = document.querySelector("#hoverDos");
+const listElements = document.querySelectorAll(".list__button--click");
 
-const verMas = document.querySelector("#verMas");
-/*
-function agregarBoton (){
-        verMas.innerHTML=`<input type="button" value="Ver Mas"></input>`
-        const claseAAgregar = 'inputClase';
-        verMas.classList.add(claseAAgregar);   
-}
-agregarBoton(verMas)
-const inputClase = document.querySelector(".inputClase")
-
-inputClase.addEventListener("click",()=>{
-        hoverUno.style.backgroundImage = "url(/Archivos/imagenes-card-final/imagenes-hover/imagen-1.png)";
-        hoverUno.style.backgroundRepeat= "no-Repeat" 
-        hoverUno.style.backgroundPosition= "center" ;
-        hoverUno.style.backgroundSize="auto" ;
-        hoverUno.style.opacity= "1" ;
-        hoverUno.style.cursor= "pointer" ;
-        hover.style.opacity= "1" 
-        hover.style.transition="transform 0.3s, opacity 0.3s" 
-})*/
+listElements.forEach(listElement => {
+        listElement.addEventListener("click",()=>{
+                listElement.classList.toggle("arrow");
+                let height = 0;
+                let menu = listElement.nextElementSibling;
+                if (menu.clientHeight =="0"){
+                        height = menu.scrollHeight;
+                } 
+                menu.style.height =`${height}px`;
+        })
+});
